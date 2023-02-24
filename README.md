60031f05fc4638c9f66db5a261d44fccafed3931
Fabio de Stefani <fabiostefani@gmail.com>
2023-02-23 22:09:44 -0300

    exemplo com stryker

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Stryker-Mutation_Net
 Stryker Mutation
+
+dotnet tool install -g dotnet-stryker
+
+dotnet stryker --mutation-level complete --open-report
---
+++ StrykerMutationTests/Core/Person/PersonTests.cs
@@ -0,0 +1,56 @@
+﻿using CoreEntity = StrykerMutation.Core.Person;
+using FluentAssertions;
+
+namespace StrykerMutationTests.Core.Person;
+
+public class PersonTests
+{
+    [Fact]
+    public void Instantiate()
+    {
+        const string validName = "Fabio de Stefani";
+        const int validAge = 38;
+
+        var action = () => new CoreEntity.Person(validName, validAge);
+        
+        var person = action.Should().NotThrow().Which;
+        person.Id.Should().NotBeEmpty();
+        person.Age.Should().Be(validAge);
+        person.Name.Should().Be(validName);
+    }
+    
+    [Theory]
+    [InlineData(14, false)]
+    [InlineData(29, true)]
+    [InlineData(17, false)]
+    [InlineData(18, true)]
+    public void OverMajorityAgeShouldReturnProperly(
+        int age, 
+        bool expectedResult)
+    {
+        var person = new CoreEntity.Person("Fabio de Stefani", age);
+        var result = person.IsOverMajorityAge();
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("Invalid")]
+    [InlineData(null)]
+    public void WhenInstantiatingInvalidNameShouldThrow(string invalidName)
+    { 
+        var action = () => new CoreEntity.Person(invalidName, 38);
+        action.Should().Throw<Exception>()
+            .WithMessage("Name should be composed by name and surname");
+        
+    }
+    
+    [Theory]
+    [InlineData("Fabio Stefani")]
+    [InlineData("Fabio de Stefani")]
+    public void WhenInstantiatingValidNameShouldNotThrow(string validName)
+    { 
+        var action = () => new CoreEntity.Person(validName, 38);
+        action.Should().NotThrow();
+        
+    }
+}
